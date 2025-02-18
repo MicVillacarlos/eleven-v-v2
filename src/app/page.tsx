4be2f2ebbef8346faa9fba46f0a6035d70303ec9
MicVillacarlos <micvillacarlos@gmail.com>
2025-02-19 01:19:05 +0800

    add middleware to check validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,16 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
 
-  useEffect(() => {
-    const token = localStorage.getItem("authToken");
+  // useEffect(() => {
+  //   const token = localStorage.getItem("authToken");
 
-    if (token) {
-      router.replace("/admin/dashboard");
-    } else {
-      router.replace("/login");
-      localStorage.removeItem("authToken")
-    }
-  }, [router]);
+  //   if (token) {
+  //     router.replace("/admin/dashboard");
+  //   } else {
+  //     router.replace("/login");
+  //     localStorage.removeItem("authToken")
+  //   }
+  // }, [router]);
 
   return <div className="h-screen w-screen flex item-center justify-center">Redirecting...</div> 
 }
---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const token = req.cookies.get("authToken")?.value;
+  const loginUrl = new URL("/login", req.url);
+
+  if (!token) {
+    console.log("No token found, redirecting to login");
+    return NextResponse.redirect(loginUrl);
+  }
+
+  try {
+    const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+    const response = await fetch(`${backendUrl}/admin/auth/validate-token/${token}`);
+    const data = await response.json();
+    console.log("API response:", data);
+
+    if (!data.success) {
+      return NextResponse.redirect(loginUrl);
+    }
+  } catch (error) {
+    console.error("Token validation error:", error);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  console.log("Token is valid, proceeding...");
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/((?!api|_next/static|_next/image|favicon.ico|login).*)",
+};
