17b62ff2ac819f269353672b60c8ed5198391ca7
MicVillacarlos <micvillacarlos@gmail.com>
2025-02-21 13:10:07 +0800

    Fix Login UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const router = useRouter();
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem("authToken");
-
-  //   if (token) {
-  //     router.replace("/admin/dashboard");
-  //   } else {
-  //     router.replace("/login");
-  //     localStorage.removeItem("authToken")
-  //   }
-  // }, [router]);
-
   return <div className="h-screen w-screen flex item-center justify-center">Redirecting...</div> 
 }
