25fa18b85915a578bf4b0446dee10461561f8a47
MicVillacarlos <micvillacarlos@gmail.com>
2025-02-21 13:46:53 +0800

    clean Layout code and move hamburger icon to svg-icons folder

--- src/app/components/Organisms/Layout.tsx
+++ src/app/components/Organisms/Layout.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { useWindowSize } from "../../utils/hooks";
-import { useRouter } from "next/navigation";
 import { DashboardIcon } from "../svg/DashboardIcon";
+import { HamburgerIcon } from "../svg/HamburgerIcon";
 
 export default function Layout({
   children,
@@ -11,7 +11,6 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   const windowWidth: number = useWindowSize();
-  const router = useRouter();
   const [isSideBarOpen, setIsSidebarOpen] = useState<boolean>(false);
 
   useEffect(() => {
@@ -26,8 +25,24 @@ export default function Layout({
     setIsSidebarOpen(!isSideBarOpen);
   };
 
+  const SideBarOptions = [
+    {
+      key: 1,
+      label: "Dashboard",
+      href: "dashboard",
+      icon: <DashboardIcon />,
+    },
+    {
+      key: 2,
+      label: "Settings",
+      href: "settings",
+      icon: <DashboardIcon />,
+    },
+  ];
+
   return (
     <div>
+      {/* ----------SIDE BAR CLOSE (to refactor)-------- */}
       <button
         data-drawer-target="logo-sidebar"
         data-drawer-toggle="logo-sidebar"
@@ -36,22 +51,9 @@ export default function Layout({
         className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         onClick={toggleOpenSidebar}
       >
-        <span className="sr-only">Open sidebar</span>
-        <svg
-          className="w-6 h-6"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            clipRule="evenodd"
-            fillRule="evenodd"
-            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-          ></path>
-        </svg>
+        <HamburgerIcon />
       </button>
-
+      {/* ----------SIDE BAR OPEN-------- */}
       <aside
         id="logo-sidebar"
         className={`fixed top-0 left-0 z-40 w-64 h-screen transition-transform ${
@@ -66,43 +68,19 @@ export default function Layout({
             </span>
           </a>
           <ul className="space-y-2 font-medium">
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-              >
-                <DashboardIcon />
-                <span className="ms-3">Dashboard</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-              >
-                <svg
-                  className="shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
-                  <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
-                  <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
-                </svg>
-                <span className="flex-1 ms-3 whitespace-nowrap">Lodgers</span>
-              </a>
-            </li>
-            <li>
-              <a
-                href="settings"
-                className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
-              >
-                <DashboardIcon />
-                <span className="ms-3">Settings</span>
-              </a>
-            </li>
+            {SideBarOptions.map((item) => {
+              return (
+                <li key={item.key}>
+                  <a
+                    href={item.href}
+                    className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  >
+                    {item.icon}
+                    <span className="ms-3">{item.label}</span>
+                  </a>
+                </li>
+              );
+            })}
           </ul>
         </div>
       </aside>
