a9cad70138a4fb34c542f118d1780ce47c5ddd65
MicVillacarlos <micvillacarlos@gmail.com>
2025-02-21 13:35:02 +0800

    remove column broken line

--- src/app/components/Organisms/Layout.tsx
+++ src/app/components/Organisms/Layout.tsx
@@ -108,7 +108,7 @@ export default function Layout({
       </aside>
 
       <div className="p-4 sm:ml-64">
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg mt-14">
+        <div className="p-4 mt-10">
           <div className="grid grid-cols-3 gap-4 mb-4">{children}</div>
         </div>
       </div>
