ca67041fe6423ba225291b3f198834b0265966ed
MicVillacarlos <micvillacarlos@gmail.com>
2025-02-25 17:59:31 +0800

    adjust size

--- src/app/components/Atoms/buttons/PrimaryButton.tsx
+++ src/app/components/Atoms/buttons/PrimaryButton.tsx
@@ -12,7 +12,7 @@ const PrimaryButton = (props: PrimaryButtonProps) => {
   return (
     <button
       type={type}
-      className={`text-white bg-[#205072] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 ${
+      className={`text-white bg-[#205072] hover:bg-blue-800 focus:ring-4 font-semibold focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 ${
         width ?? "w-full"
       }`}
       onClick={onClick}
