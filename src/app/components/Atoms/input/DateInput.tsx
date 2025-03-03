92844ffe1121a566e6f55e03e545b3507e7a2b8d
MicVillacarlos <micvillacarlos@gmail.com>
2025-03-04 00:01:02 +0800

    Fix dateInput

--- src/app/components/Atoms/input/DateInput.tsx
+++ src/app/components/Atoms/input/DateInput.tsx
@@ -1,37 +1,37 @@
 import moment from 'moment-timezone';
-import React from "react";
+import React, { useState } from "react";
 import { config } from "../../../../config/config";
 
 interface DateInputProps {
   id: string;
   label?: string;
-  placeholder: string;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   required?: boolean;
   value: string;
-  minDate?: string
+  placeholder: string;
+  minDate?: string;
 }
 
-const DateInput = ({ id, label, placeholder, handleChange, required = false, value, minDate }: DateInputProps) => {
+const DateInput = ({ id, label, handleChange, required = false, value, minDate, placeholder}: DateInputProps) => {
+  const [inputType, setInputType] = useState("text");
 
   return (
     <div className="mb-6">
       {label && (
-        <label
-          htmlFor={id}
-          className="block mb-2 text-sm font-medium text-gray-900"
-        >
+        <label htmlFor={id} className="block mb-2 text-sm font-medium text-gray-900">
           {label}
         </label>
       )}
       <div className="relative max-w-sm">
         <input
           id={id}
-          type="date"
+          type={inputType}
+          onFocus={() => setInputType("date")}
+          onBlur={() => !value && setInputType("text")}
           className="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 appearance-none"
-          placeholder={placeholder}
           onChange={handleChange}
           required={required}
+          placeholder={placeholder}
           value={value}
           max={minDate ?? moment().tz(config.timezone!).format("YYYY-MM-DD")}
         />
