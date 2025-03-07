29e5ad1df273ea5706baa50044b8faef4ef34a78
MicVillacarlos <micvillacarlos@gmail.com>
2025-03-07 08:40:53 +0800

    fetch data

--- src/app/components/Organisms/loaders/TableLoading.tsx
+++ src/app/components/Organisms/loaders/TableLoading.tsx
@@ -3,15 +3,13 @@ import Loadingbar from "../../Atoms/loading/Loadingbar";
 
 const TableLoading = () => {
   return (
-    <div className="w-full flex bg-gray-100 flex-col gap-3 p-5 animate-pulse rounded rounded-lg">
+    <div className="w-full bg-transparent flex flex-col gap-3 p-5 animate-pulse rounded rounded-lg">
       <div className="mb-3">
-        <Loadingbar />
+        <Loadingbar color="bg-gray-400"/>
       </div>
-      <Loadingbar />
-      <Loadingbar />
-      <Loadingbar />
-      <Loadingbar />
-      <Loadingbar />
+      <Loadingbar color="bg-gray-300" />
+      <Loadingbar color="bg-gray-200" />
+      <Loadingbar color="bg-gray-200" />
     </div>
   );
 };
