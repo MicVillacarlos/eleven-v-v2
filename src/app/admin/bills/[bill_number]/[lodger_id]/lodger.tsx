64921ba456808f5ce4fbdb86daf870fed11f47d4
MicVillacarlos <micvillacarlos@gmail.com>
2025-03-27 16:28:21 +0800

    add bill message page

--- src/app/admin/bills/[bill_number]/[lodger_id]/lodger.tsx
+++ src/app/admin/bills/[bill_number]/[lodger_id]/lodger.tsx
@@ -1,11 +1,15 @@
-"use client"
-import React, { JSX } from "react";
+"use client";
+import React, { JSX, useCallback, useState } from "react";
 import Layout from "../../../../components/Organisms/layout/Layout";
 import Text3xl from "../../../../components/Atoms/text/Text3xl";
 import TableLoading from "../../../../components/Organisms/loaders/TableLoading";
 import dynamic from "next/dynamic";
-import { TableProps } from "../../../../components/Organisms/table/type";
+import {
+  Column,
+  TableProps,
+} from "../../../../components/Organisms/table/type";
 import TextLarge from "../../../../components/Atoms/text/TextLarge";
+import { Bill } from "../../../../../lib/admin/api/bills/types";
 
 //---Start---Note: Use `dynamic`(Next Js for Lazy Loading) for components fetching data. This is for optimization
 const LodgerTable = dynamic(
@@ -14,60 +18,101 @@ const LodgerTable = dynamic(
     loading: () => <TableLoading />,
     ssr: false,
   }
-) as <T extends { _id: string; }>(props: TableProps<T>) => JSX.Element;
+) as <T extends { _id: string }>(props: TableProps<T>) => JSX.Element;
 //---End---Note: Use `dynamic`(Next Js for Lazy Loading) for components fetching data. This is for optimization
 
-const Lodger = () => {
+const Lodger = ({
+  initialBills,
+  initialTotal,
+}: {
+  initialBills: Bill[];
+  initialTotal: number;
+}) => {
+  const [billsTableData, setBillsTableData] = useState<Bill[]>(initialBills);
+
+  const [pagination, setPagination] = useState({
+    current: 1,
+    limit: 10,
+    total: initialTotal,
+  });
+
+  const tableColumns: Column<Bill>[] = [
+    { key: "bill_number", label: "Bill No." },
+    { key: "room_number", label: "Room" },
+    {
+      key: "lodger_full_name",
+      label: "Lodger Name",
+    },
+    { key: "due_date", label: "Due Date", type: "date" },
+    { key: "type_of_bill", label: "Bill Type" },
+    { key: "status", label: "Status", type: "status_select" },
+    { key: "bill_amount", label: "Amount", type: "money", justify: "right" },
+  ];
+
+  const handleNextPagination = useCallback(() => {
+    setPagination((prevState) => {
+      const { total, limit, current } = prevState;
+      const lastPage = Math.ceil(total / limit);
+      return current < lastPage
+        ? { ...prevState, current: current + 1 }
+        : prevState;
+    });
+  }, []);
+
+  const handlePrevPagination = useCallback(() => {
+    setPagination((prevState) => ({
+      ...prevState,
+      current: Math.max(1, prevState.current - 1),
+    }));
+  }, []);
+
   return (
     <Layout>
       {/* -------------- Header Table--------------*/}
       <div className="flex w-full justify-between items-center mb-3">
         <Text3xl>Send Bill Notification</Text3xl>
       </div>
-      <div>
-        <TextLarge> You are about to send this bill to User</TextLarge>
+      <div className="flex gap-1 items-center">
+        <p className="text-gray-900"> You are about to send this bill to</p>
+        <TextLarge>Michael</TextLarge>
       </div>
 
-      <div>
-        <TextLarge>
-          To attach additional bills, simply click on the ones you want to
-          include from the table below.
-        </TextLarge>
-      </div>
+      {billsTableData.length && (
+        <>
+          <div>
+            <p className="text-gray-900">
+              To attach additional bills, simply click on the ones you want to
+              include from the table below.
+            </p>
+          </div>
 
-      {/* -------------- Header Table--------------*/}
-      <div className="flex w-full justify-between mb-6 items-center gap-5 mb-5">
-        {/* <div className="lg:w-[150px]">
-          <FilterTableButton
-            onSelectFilter={function (): void {
+          {/* -------------- Header Table--------------*/}
+          <div className="flex w-full justify-between mb-6 items-center gap-5 mb-5">
+            {/* <div className="lg:w-[150px]">
+             <FilterTableButton
+               onSelectFilter={function (): void {
+                 throw new Error("Function not implemented.");
+               }}
+               options={[]}
+               filterValue={{}}
+               onClickReset={function (): void {
+                 throw new Error("Function not implemented.");
+               }}
+             />
+           </div> */}
+          </div>
+          <LodgerTable
+            data={billsTableData}
+            columns={tableColumns}
+            handleNextNavigation={function (): void {
               throw new Error("Function not implemented.");
             }}
-            options={[]}
-            filterValue={{}}
-            onClickReset={function (): void {
-              throw new Error("Function not implemented.");
-            }}
+            handlePrevNavigation={handlePrevPagination}
+            onSelectTablePage={handleNextPagination}
+            pagination={pagination}
           />
-        </div> */}
-      </div>
-      <LodgerTable
-        data={[]}
-        columns={[]}
-        handleNextNavigation={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-        handlePrevNavigation={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-        onSelectTablePage={function (page: number): void {
-          throw new Error("Function not implemented.");
-        }}
-        pagination={{
-          current: 0,
-          limit: 0,
-          total: 0,
-        }}
-      />
+        </>
+      )}
     </Layout>
   );
 };
--- src/app/admin/bills/[bill_number]/[lodger_id]/page.tsx
+++ src/app/admin/bills/[bill_number]/[lodger_id]/page.tsx
@@ -1,27 +1,18 @@
+import { fetchBillsMessagingInitial } from "../../../../../lib/admin/api/bills/bills-server";
 import Lodger from "./lodger";
-
-export default async function LodgerId() {
-  // const { data: initialLodger, count: initialTotal } = await fetchRoomsInitial(
-  //   "",
-  //   1,
-  //   5
-  // );
-
-  return <Lodger />;
+interface LodgerMessagingProps {
+  params: {
+    bill_number: string;
+    lodger_id: string;
+  };
 }
 
+export default async function LodgerId({params}:LodgerMessagingProps) {
+  const { bill_number, lodger_id } = params;
+  const billNumberFiltered = Number(bill_number.slice(2));
 
-// import Lodger from "./lodger";
-// import { fetchRoomsInitial } from "@/lib/api"; // Adjust path if needed
+  const { data: initialBills, count: initialTotal } =
+    await fetchBillsMessagingInitial(billNumberFiltered, lodger_id, 1, 5);
 
-// export default async function LodgerId({ params }: { params: { bill_id: string } }) {
-//   const { bill_id } = params;
-
-//   const { data: initialRooms, count: initialTotal } = await fetchRoomsInitial(
-//     bill_id, // Use bill_id from params
-//     1,
-//     5
-//   );
-
-//   return <Lodger />;
-// }
+  return <Lodger initialBills={initialBills} initialTotal={initialTotal} />;
+}
